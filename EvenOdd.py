6344eff80873de5a0ec2202f744f47137dbe2e7a
LeoSuave25 <suaverdez90@gmail.com>
2023-04-22 21:50:31 +0800

    Updated the Variable Name to number for better understanding

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,11 +1,12 @@
 #Write a Python program that reads a text file named numbers.txt that contains 20 integers
-with open('numbers.txt','r') as rawinput_text, open('even.txt','a') as output_even, open('odd.txt','a') as output_odd:
+with open('numbers.txt','r') as rawinput_text, open('even.txt','w') as output_even, open('odd.txt','w') as output_odd:
 #The program will create two other text file the first text file will be named even.txt
 # It will contains all even numbers extracted from the numbers.txt
     raw_numbers=rawinput_text.readlines()
 
     for line in raw_numbers:
-        if int(line) % 2 == 0:
-            output_even.write(line)
+        number = int(line.strip())
+        if number % 2 == 0:
+            output_even.write(str(number)+'\n')
 #The second text file will be named odd.txt that will
 #It will contain all odd numbers extracted from the numbers.txt
