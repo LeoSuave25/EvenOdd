e6db10a05fd1569327be2cbc1b624f61d34109e7
LeoSuave25 <suaverdez90@gmail.com>
2023-04-22 21:22:50 +0800

    Pseudocode

---
+++ EvenOdd.py
@@ -0,0 +1,5 @@
+#Write a Python program that reads a text file named numbers.txt that contains 20 integers
+#The program will create two other text file the first text file will be named even.txt
+    # It will contains all even numbers extracted from the numbers.txt
+#The second text file will be named odd.txt that will
+    #It will contain all odd numbers extracted from the numbers.txt
